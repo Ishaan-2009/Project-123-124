0382282b4a511c3b1532880f6a169fb08d7689b5
Ishaan-2009 <95971642+Ishaan-2009@users.noreply.github.com>
2023-08-04 10:17:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Initialized!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+    
+    }
+}
+
+function draw() {
+    background('#33E9FF');
+    textSize(difference/2);
+    fill('#FF3C33');
+    stroke('#FF3C33');
+    text('Ishaan', 50, 400);
+}
